655581e711b07234365b4d27cf87904dcb69d076
Dimitris Nikolaou <dimitrisnikolaou10@gmail.com>
2022-05-13 22:08:26 +0100

    adding form for more tests

--- test/hipmunks.js
+++ test/hipmunks.js
@@ -1,6 +1,14 @@
 const Hipnation = artifacts.require("Hipnation");
 
 contract("First hipmunks test", async accounts => {
+
+/*
+Tests
+- DaoAdmin cannot be set by non owner
+- DaoAdmin can be set by owner
+- DaoAdmin can be set by admin
+*/
+
   it("sample test should pass", async () => {
     const instance = await Hipnation.deployed();
     const balance = await instance.getBalance.call(accounts[0]);
