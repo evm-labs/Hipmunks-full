2b98a13239cf45e062fd615ed5bcf65dcaa733a7
Dimitris Nikolaou <dimitrisnikolaou10@gmail.com>
2022-05-23 16:36:44 -0400

    adding more tests for Hipmunks.sol

--- test/hipmunks.js
+++ test/hipmunks.js
@@ -10,16 +10,15 @@ Tests completed
 - Anyone can mint sale once open
 - Only owner can mint reserved
 - Owner can only reserve once (or twice)
-- Cannot mint preSale before time
-
+- Owner and only owner can set URIs
+- Only owner can mint donated
+- Only owner can deposit to Dao
 
 
 Tests to add
-- Owner (and only owner) can set the URIs
+- Cannot mint preSale before time
 - Only whitelist can mint presale once open
 - Cannot mint presale & sale more than amount
-- Only owner can mint donated
-- Only owner can deposit to Dao
 - Only owner can withdraw
 
 */
@@ -136,5 +135,72 @@ Tests to add
   })
 
 
+  it("Owner and only owner can set URIs", async () => {
+    const contract = await Hipmunks.deployed();
+    const contractOwner = await contract.owner.call();
+    const randomAccount = await accounts[1];
+
+    await truffleAssert.reverts(
+      contract.setTokenBaseURI("test", {from: randomAccount}),
+      "Ownable: caller is not the owner"
+    );
+
+    await contract.setTokenBaseURI("test", {from: contractOwner});
+    tokenBaseURI = await contract.tokenBaseURI();
+
+    assert.equal(
+      tokenBaseURI,
+      "test",
+      "Owner did not change the token URI"
+    );
+  })
+
+  it("Only owner can mint donated and can only do it once", async () => {
+    const contract = await Hipmunks.deployed();
+    const contractOwner = await contract.owner.call();
+    const randomAccount = await accounts[1];
+
+    await truffleAssert.reverts(
+      contract.mintDonatedChipmunks({from: randomAccount}),
+      "Ownable: caller is not the owner"
+    );
+
+    tokensBefore = await contract.tokenSupply();
+    await contract.mintDonatedChipmunks({from: contractOwner});
+    tokensAfter = await contract.tokenSupply();
+
+    assert.equal(
+      tokensAfter - tokensBefore,
+      22,
+      "Donations were not minted"
+    );
+
+    await truffleAssert.reverts(
+      contract.mintDonatedChipmunks({from: contractOwner}),
+      "Donations have already been minted."
+    );
+  })
+
+  it("Only owner can deposit to Dao", async () => {
+    const contract = await Hipmunks.deployed();
+    const contractOwner = await contract.owner.call();
+    const randomAccount = await accounts[1];
+
+    await contract.fallback({from: randomAccount, value: 1000000000000000000});
+
+    await truffleAssert.reverts(
+      contract.depositToDao({from: randomAccount}),
+      "Ownable: caller is not the owner"
+    );
+
+    await contract.setTokenBaseURI("test", {from: contractOwner});
+    tokenBaseURI = await contract.tokenBaseURI();
+
+    assert.equal(
+      tokenBaseURI,
+      "test",
+      "Owner did not change the token URI"
+    );
+  })
 
 });
